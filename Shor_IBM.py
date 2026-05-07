92f7773f43732c2dd1f3580eb85be8e2284d8f29
Quantum <mkhum4n@gmail.com>
2026-05-07 13:58:17 +0000

    Importing

---
+++ Shor_IBM.py
@@ -0,0 +1,6 @@
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+import numpy as np
+
