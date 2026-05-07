7284bd931329cdaec49732279320e91f8586ace9
Quantum <mkhum4n@gmail.com>
2026-05-07 14:00:23 +0000

    Quantum Circuit

--- Shor_IBM.py
+++ Shor_IBM.py
@@ -4,3 +4,22 @@ from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+def qft_dagger(n):
+    """
+    Inverse Quantum Fourier Transform
+    """
+    qc = QuantumCircuit(n)
+
+    
+    for qubit in range(n // 2):
+        qc.swap(qubit, n - qubit - 1)
+
+    
+    for j in range(n):
+        for m in range(j):
+            qc.cp(-np.pi / float(2 ** (j - m)), m, j)
+
+        qc.h(j)
+
+    return qc
